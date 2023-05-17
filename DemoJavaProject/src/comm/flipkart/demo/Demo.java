c73f789342f2d16dec17d100000b3ee025f185f5
Aditya Gupta <aditya.gupta6@BLRETV-L0TYMD6M.local>
2023-05-17 17:29:28 +0530

    first commit

---
+++ DemoJavaProject/src/comm/flipkart/demo/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package comm.flipkart.demo;
+
+/**
+ * @author aditya.gupta6
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my first program.\n");
+	}
+
+}
